2ca2a0d9aa27ea9c58470de5259eb7e996214b68
Marcos Roberto <marcosprojetosdev@gmail.com>
2023-08-02 15:02:41 -0300

    :sparkles: create styles config stitches

---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,36 @@
+import { createStitches, defaultThemeMap } from '@stitches/react'
+import {
+  colors,
+  space,
+  radii,
+  fonts,
+  fontSizes,
+  fontWeights,
+  lineHeights,
+} from '@marcos-roberto-dev/tokens'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    height: 'space',
+    width: 'space',
+  },
+  theme: {
+    colors,
+    space,
+    radii,
+    fonts,
+    fontSizes,
+    fontWeights,
+    lineHeights,
+  },
+})
