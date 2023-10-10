f15fc890e930ebd6da4ca5c135c0cec7c10ce741
Marcos Roberto <marcosprojetosdev@gmail.com>
2023-10-10 08:16:55 -0300

    :sparkles: create textarea component

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,27 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, Text, TextArea, TextAreaProps } from '@marcos-roberto-dev/react'
+
+export default {
+  title: 'Form/Text Area',
+  component: TextArea,
+  args: {
+    disabled: false,
+    placeholder: 'Add any observation...',
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text>Observation</Text>
+          <Story />
+        </Box>
+      )
+    },
+  ],
+  tags: ['autodocs'],
+} as Meta<TextAreaProps>
+
+export const Primary: StoryObj<TextAreaProps> = {}
