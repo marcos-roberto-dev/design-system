d1c382ef158fb00c0d6f5a7936146a814739970b
Marcos Roberto <marcosprojetosdev@gmail.com>
2023-10-13 11:12:12 -0300

    :sparkles: create MultiStep component

---
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -0,0 +1,36 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, MultiStep, MultiStepProps } from '@marcos-roberto-dev/react'
+
+export default {
+  title: 'Form/Multi Step',
+  component: MultiStep,
+  args: {
+    size: 4,
+    currentStep: 1,
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '$2',
+          }}
+        >
+          <Story />
+        </Box>
+      )
+    },
+  ],
+  tags: ['autodocs'],
+} as Meta<MultiStepProps>
+
+export const Primary: StoryObj<MultiStepProps> = {}
+
+export const Full: StoryObj<MultiStepProps> = {
+  args: {
+    currentStep: 4,
+  },
+}
