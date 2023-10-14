591b0d940f4ddfaeb05d960fa67909ed64c8f33d
Marcos Roberto <marcosprojetosdev@gmail.com>
2023-10-14 15:02:58 -0300

    :recycle: Add ref to TextInput and adjust styles

--- packages/docs/src/stories/TextInput.stories.tsx
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -17,6 +17,7 @@ export const Primary: StoryObj<TextInputProps> = {
 export const WithPrefix: StoryObj<TextInputProps> = {
   args: {
     prefix: 'cal.com/',
+    placeholder: 'your-username'
   },
 }
 
